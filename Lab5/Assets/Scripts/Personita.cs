85bf1a3d21375ea3573e07b1566d8d11c7d6541c
Cristina B <bau161260@uvg.edu.gt>
2019-04-23 07:16:10 -0600

    Ya tiene miragit status

---
+++ Lab5/Assets/Scripts/Personita.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personita : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Input.GetMouseButtonDown(0))
+            if(Physics.Raycast(myRay, out hitInfo))
+            {
+                if (hitInfo.collider.gameObject.CompareTag("Blancos"))
+                {
+                    Destroy(hitInfo.collider.gameObject);
+                }
+            }
+    }
+}
